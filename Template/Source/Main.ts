1812400d5004435d2c581a94ec0f08dbef55fff5
Vs-Rev <guvas.va@gmail.com>
2022-10-20 23:32:00 +0200

    Designstuff added

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -13,4 +13,39 @@ namespace Template {
     // start the sequence
     ƒS.Progress.go(scenes);
   }
+
+  export let transition = {
+    puzzle: {
+      duration: 1,
+      alpha: "",
+      edge: 1
+    }
+  }
+  export let locations = {
+    beachDay: {
+      name: "Beach Day",
+      background: "Tutorial_SS22/Images/Backgrounds/Bathroom_Foggy.png"
+    }
+  }
+
+  export let sound = {
+    drop: "Audio/drop.mp3"
+  };
+
+  export let characters = {
+    narrator: {
+      name: ""
+
+    }
+     protagonist: {
+      name: ""
+     },
+     Lucor: {
+      name: "Lucor",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        
+      }
+     }
+  }
 }
